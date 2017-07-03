f76989b9bf9aeebf245c0c8ae6c9c7adb046771f
Peter Burkholder <peter.burkholder@gsa.gov>
2017-07-03 13:46:40 -0400

    Self-documenting variables

--- ci/task_hello_world.sh
+++ ci/task_hello_world.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo FOO: $FOO
-echo BAR: $BAR
-echo BAZ: $BAZ
+echo TASK: ${TASK:-value not provided by task.yml}
+echo PIPE: ${PIPE:-value not provided by pipeline.yml}
+echo CRED: ${CRED:-value not provided by credentials.yaml}
+echo VAULT: ${VAULT:-value not provided by vault}
